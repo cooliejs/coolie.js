fd487e077c60787813fbe287e4645a334af386a3
cloudcome <cloudcome@163.com>
2014-11-20 11:36:18 +0800

    更新示例

--- example/anonymous/index.js
+++ example/anonymous/index.js
@@ -1,6 +1,8 @@
 define(function (require) {
     console.log('123');
     var a = require('../a');
+    var text = require('text!../text/some.txt');
     console.log('456');
+    console.log(text);
     a();
 });
