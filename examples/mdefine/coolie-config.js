15f4b16520fc47d56db8e64e156ac4f1db59062c
张云来 <cloudcome@163.com>
2015-06-25 19:35:56 +0800

    chunk

--- examples/mdefine/coolie-config.js
+++ examples/mdefine/coolie-config.js
@@ -1,8 +1,8 @@
 coolie.config({
     base: './',
     version: {
-        '0': 'abc123',
-        '1': 'abc123',
+        '0.js': 'abc123',
+        '1.js': 'abc123',
         'main.js': 'def456'
     }
 }).use();
