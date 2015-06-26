78093796f2aab09c5827667694017d7ff80a3985
张云来 <cloudcome@163.com>
2015-06-26 12:02:56 +0800

    chunk

--- coolie.js
+++ coolie.js
@@ -1,7 +1,7 @@
 /*!
  * coolie 苦力
  * @author ydr.me
- * @version 0.14.3
+ * @version 0.14.4
  * @license MIT
  */
 
@@ -47,7 +47,7 @@
      * coolie 版本号
      * @type {string}
      */
-    coolie.version = '0.14.3';
+    coolie.version = '0.14.4';
 
 
     /**
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# coolie.js@0.14.3 苦力 
+# coolie.js@0.14.4 苦力 
 [![Build Status][travis-img]][travis-url] 
 [![Coverage Status][coveralls-img]][coveralls-url]
 
