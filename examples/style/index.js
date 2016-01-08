16a5b6f99a6fcb82b5eaecfd4169761f705834ca
cloudcome <cloudcome@163.com>
2016-01-08 12:15:37 +0800

    + style type

---
+++ examples/style/index.js
@@ -0,0 +1,18 @@
+/**
+ * 文件描述
+ * @author ydr.me
+ * @create 2016-01-08 12:13
+ */
+
+
+define(function (require, exports, module) {
+    /**
+     * @module parent/index
+     */
+
+    'use strict';
+
+    var style = require('./style.css', 'css|style');
+
+    console.log(style);
+});
