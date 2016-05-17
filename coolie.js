954919c34729da1cbe510b43c48c14f47e80bc4b
cloudcome <cloudcome@163.com>
2016-05-17 13:27:56 +0800

    2.x

--- coolie.js
+++ coolie.js
@@ -1,7 +1,7 @@
 /**
  * coolie 苦力
  * @author coolie.ydr.me
- * @version 2.0.0-alpha1
+ * @version 2.0.0-alpha2
  * @license MIT
  */
 
@@ -9,7 +9,7 @@
 ;(function () {
     'use strict';
 
-    var VERSION = '2.0.0-alpha1';
+    var VERSION = '2.0.0-alpha2';
     var COOLIE = 'coolie';
     var NODE_MODULES = 'node_modules';
     var JS = 'js';
