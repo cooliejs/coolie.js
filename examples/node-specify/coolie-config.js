64fe45bcae253590a4e2ff651ae92fca6a33da42
cloudcome <cloudcome@163.com>
2016-05-24 10:55:29 +0800

    nodeModuleMainPath

--- examples/node-specify/coolie-config.js
+++ examples/node-specify/coolie-config.js
@@ -1,5 +1,5 @@
 coolie.config({
     mainModulesDir: './examples/node-specify/',
     nodeModulesDir: '/examples/node-specify/node_modules/',
-    nodeModulePath: './src/index.js'
+    nodeModuleMainPath: './src/index.js'
 }).use();
